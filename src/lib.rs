5231bd08efa5b391e4e554a7e455746b14c85433
John-John Tedro <udoprog@tedro.se>
2023-03-22 17:10:35 +0100

    Release 0.6.4

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@
 //! Add `checkers` as a dev-dependency to your project:
 //!
 //! ```toml
-//! checkers = "0.6.3"
+//! checkers = "0.6.4"
 //! ```
 //!
 //! Replace the global allocator in a [test file] and wrap tests you wish to
